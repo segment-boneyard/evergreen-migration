226169913495dfcb56c474a7d597676acc3a3c49
Roland Warmerdam <roland@segment.com>
2018-01-29 21:33:01 -0800

    Remove semicolons

--- test/cli.js
+++ test/cli.js
@@ -1,10 +1,10 @@
-import path from "path";
-import test from "ava";
-import { readFile, writeFile } from "sander";
-import tempy from "tempy";
-import execa from "execa";
+import path from 'path'
+import test from 'ava'
+import { readFile, writeFile } from 'sander'
+import tempy from 'tempy'
+import execa from 'execa'
 
-const cliPath = path.join(__dirname, "../lib/cli.js");
+const cliPath = path.join(__dirname, '../lib/cli.js')
 const fixture = `
 import React from 'react'
 import {
@@ -15,51 +15,51 @@ import TextInput from 'evergreen-text-input'
 import SelectMenu from 'evergreen-select-menu'
 import { Text } from 'evergreen-typography'
 import DropdownButton from './DropdownButton'
-`;
+`
 const expectedOutput = `
 import React from 'react'
 import { Pane as EGPane, Card, TextInput, SelectMenu, Text } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`;
+`
 
-test("transforms a single file", async t => {
-  const outputPath = tempy.file();
-  await writeFile(outputPath, fixture);
-  await execa(cliPath, [outputPath]);
+test('transforms a single file', async t => {
+  const outputPath = tempy.file()
+  await writeFile(outputPath, fixture)
+  await execa(cliPath, [outputPath])
 
-  t.is(await readFile(outputPath, { encoding: "utf-8" }), expectedOutput);
-});
+  t.is(await readFile(outputPath, { encoding: 'utf-8' }), expectedOutput)
+})
 
-test("transforms a glob of files", async t => {
-  const outputDirectory = tempy.directory();
-  await writeFile(outputDirectory, "test1.js", fixture);
-  await writeFile(outputDirectory, "test2.js", fixture);
-  await execa(cliPath, [`${outputDirectory}/test*.js`]);
+test('transforms a glob of files', async t => {
+  const outputDirectory = tempy.directory()
+  await writeFile(outputDirectory, 'test1.js', fixture)
+  await writeFile(outputDirectory, 'test2.js', fixture)
+  await execa(cliPath, [`${outputDirectory}/test*.js`])
 
   t.is(
-    await readFile(outputDirectory, "test1.js", { encoding: "utf-8" }),
+    await readFile(outputDirectory, 'test1.js', { encoding: 'utf-8' }),
     expectedOutput
-  );
+  )
   t.is(
-    await readFile(outputDirectory, "test2.js", { encoding: "utf-8" }),
+    await readFile(outputDirectory, 'test2.js', { encoding: 'utf-8' }),
     expectedOutput
-  );
-});
+  )
+})
 
-test("ignores files in node_modules", async t => {
-  const outputDirectory = tempy.directory();
-  await writeFile(outputDirectory, "test.js", fixture);
-  await writeFile(outputDirectory, "node_modules/test.js", fixture);
-  await execa(cliPath, [`${outputDirectory}/**/*.js`]);
+test('ignores files in node_modules', async t => {
+  const outputDirectory = tempy.directory()
+  await writeFile(outputDirectory, 'test.js', fixture)
+  await writeFile(outputDirectory, 'node_modules/test.js', fixture)
+  await execa(cliPath, [`${outputDirectory}/**/*.js`])
 
   t.is(
-    await readFile(outputDirectory, "test.js", { encoding: "utf-8" }),
+    await readFile(outputDirectory, 'test.js', { encoding: 'utf-8' }),
     expectedOutput
-  );
+  )
   t.is(
-    await readFile(outputDirectory, "node_modules/test.js", {
-      encoding: "utf-8"
+    await readFile(outputDirectory, 'node_modules/test.js', {
+      encoding: 'utf-8'
     }),
     fixture
-  );
-});
+  )
+})
--- test/codemod.js
+++ test/codemod.js
@@ -1,132 +1,132 @@
-import test from "ava";
-import codemod from "../lib/codemod";
+import test from 'ava'
+import codemod from '../lib/codemod'
 
-test("transforms basic import", t => {
+test('transforms basic import', t => {
   const fixture = `
 import React from 'react'
 import TextInput from 'evergreen-text-input'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { TextInput } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("handles newlines at start/end of file", t => {
+test('handles newlines at start/end of file', t => {
   const fixture = `
 import React from 'react'
 import TextInput from 'evergreen-text-input'
 import DropdownButton from './DropdownButton'
-`;
+`
   const expected = `
 import React from 'react'
 import { TextInput } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`;
+`
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("handles comment at start of file", t => {
+test('handles comment at start of file', t => {
   const fixture = `
 // eslint-disable
 import React from 'react'
 import TextInput from 'evergreen-text-input'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 // eslint-disable
 import React from 'react'
 import { TextInput } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("transforms destructure import", t => {
+test('transforms destructure import', t => {
   const fixture = `
 import React from 'react'
 import { Text } from 'evergreen-typography'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { Text } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("transforms multiple import", t => {
+test('transforms multiple import', t => {
   const fixture = `
 import React from 'react'
 import { Pane, Card } from 'evergreen-layers'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { Pane, Card } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("transforms multiple import statements", t => {
+test('transforms multiple import statements', t => {
   const fixture = `
 import React from 'react'
 import TextInput from 'evergreen-text-input'
 import SelectMenu from 'evergreen-select-menu'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { TextInput, SelectMenu } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("transforms alias import", t => {
+test('transforms alias import', t => {
   const fixture = `
 import React from 'react'
 import { Pane as EGPaneCard } from 'evergreen-layers'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { Pane as EGPaneCard } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("transforms multiline import", t => {
+test('transforms multiline import', t => {
   const fixture = `
 import React from 'react'
 import {
   Pane
 } from 'evergreen-layers'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { Pane } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("does not transform random imports", t => {
+test('does not transform random imports', t => {
   const fixture = `
 import React from 'react'
 import { Pane } from 'evergreen-layers'
@@ -135,7 +135,7 @@ import DropdownButton from './DropdownButton'
 function test() {
   return 'import { Pane } from 'evergreen-layers''
 }
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { Pane } from 'evergreen-ui'
@@ -144,12 +144,12 @@ import DropdownButton from './DropdownButton'
 function test() {
   return 'import { Pane } from 'evergreen-layers''
 }
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
 
-test("handles all the things", t => {
+test('handles all the things', t => {
   const fixture = `
 import React from 'react'
 import {
@@ -160,12 +160,12 @@ import TextInput from 'evergreen-text-input'
 import SelectMenu from 'evergreen-select-menu'
 import { Text } from 'evergreen-typography'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
   const expected = `
 import React from 'react'
 import { Pane as EGPane, Card, TextInput, SelectMenu, Text } from 'evergreen-ui'
 import DropdownButton from './DropdownButton'
-`.trim();
+`.trim()
 
-  t.is(codemod(fixture), expected);
-});
+  t.is(codemod(fixture), expected)
+})
