f2a8f88500f4cb22ff52435303b7e9e0ea87e928
Roland Warmerdam <roland@segment.com>
2018-02-04 23:15:48 -0800

    Switch from sander to fs-extra

--- test/cli.js
+++ test/cli.js
@@ -1,6 +1,6 @@
 import path from 'path'
 import test from 'ava'
-import { readFile, writeFile } from 'sander'
+import fs from 'fs-extra'
 import tempy from 'tempy'
 import execa from 'execa'
 
@@ -24,40 +24,49 @@ import DropdownButton from './DropdownButton'
 
 test('transforms a single file', async t => {
   const outputPath = tempy.file()
-  await writeFile(outputPath, fixture)
+  await fs.writeFile(outputPath, fixture)
   await execa(cliPath, [outputPath])
 
-  t.is(await readFile(outputPath, { encoding: 'utf-8' }), expectedOutput)
+  t.is(await fs.readFile(outputPath, { encoding: 'utf-8' }), expectedOutput)
 })
 
 test('transforms a glob of files', async t => {
   const outputDirectory = tempy.directory()
-  await writeFile(outputDirectory, 'test1.js', fixture)
-  await writeFile(outputDirectory, 'test2.js', fixture)
+  await fs.writeFile(path.join(outputDirectory, 'test1.js'), fixture)
+  await fs.writeFile(path.join(outputDirectory, 'test2.js'), fixture)
   await execa(cliPath, [`${outputDirectory}/test*.js`])
 
   t.is(
-    await readFile(outputDirectory, 'test1.js', { encoding: 'utf-8' }),
+    await fs.readFile(path.join(outputDirectory, 'test1.js'), {
+      encoding: 'utf-8'
+    }),
     expectedOutput
   )
   t.is(
-    await readFile(outputDirectory, 'test2.js', { encoding: 'utf-8' }),
+    await fs.readFile(path.join(outputDirectory, 'test2.js'), {
+      encoding: 'utf-8'
+    }),
     expectedOutput
   )
 })
 
 test('ignores files in node_modules', async t => {
   const outputDirectory = tempy.directory()
-  await writeFile(outputDirectory, 'test.js', fixture)
-  await writeFile(outputDirectory, 'node_modules/test.js', fixture)
+  await fs.writeFile(path.join(outputDirectory, 'test.js'), fixture)
+  await fs.outputFile(
+    path.join(outputDirectory, 'node_modules/test.js'),
+    fixture
+  )
   await execa(cliPath, [`${outputDirectory}/**/*.js`])
 
   t.is(
-    await readFile(outputDirectory, 'test.js', { encoding: 'utf-8' }),
+    await fs.readFile(path.join(outputDirectory, 'test.js'), {
+      encoding: 'utf-8'
+    }),
     expectedOutput
   )
   t.is(
-    await readFile(outputDirectory, 'node_modules/test.js', {
+    await fs.readFile(path.join(outputDirectory, 'node_modules/test.js'), {
       encoding: 'utf-8'
     }),
     fixture
@@ -66,11 +75,11 @@ test('ignores files in node_modules', async t => {
 
 test('dry run mode does not change files', async t => {
   const outputPath = tempy.file()
-  await writeFile(outputPath, fixture)
+  await fs.writeFile(outputPath, fixture)
   const result = await execa(cliPath, ['--dry-run', outputPath])
 
   t.true(result.stdout.includes(outputPath))
-  t.is(await readFile(outputPath, { encoding: 'utf-8' }), fixture)
+  t.is(await fs.readFile(outputPath, { encoding: 'utf-8' }), fixture)
 })
 
 test('handles files with no evergreen imports', async t => {
@@ -79,8 +88,8 @@ import React from 'react'
 import DropdownButton from './DropdownButton'
 `
   const outputPath = tempy.file()
-  await writeFile(outputPath, fixture)
+  await fs.writeFile(outputPath, fixture)
   await execa(cliPath, [outputPath])
 
-  t.is(await readFile(outputPath, { encoding: 'utf-8' }), fixture)
+  t.is(await fs.readFile(outputPath, { encoding: 'utf-8' }), fixture)
 })
